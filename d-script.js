737340f5262a45b305c5ef5c864a5de3544ace07
haripriya75 <haripriyaradhika28@gmail.com>
2021-09-03 21:37:53 +0530

    Added a folder Cowage

---
+++ d-script.js
@@ -0,0 +1,216 @@
+var ctx = document.getElementById('stu1-graph').getContext('2d');
+var stu1_graph = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 2,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('stu2-graph').getContext('2d');
+var stu2_graph = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 2,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('stu3-graph').getContext('2d');
+var stu3_graph = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 2,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('prof1-graph').getContext('2d');
+var prof1_graph = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 2,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('prof2-graph').getContext('2d');
+var prof2_graph = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 2,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('prof3-graph').getContext('2d');
+var prof3_graph = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 2,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('total1-graph').getContext('2d');
+var total1_graph = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 1,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('total2-graph').getContext('2d');
+var total2_graph = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [199, 19],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 1,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
+var ctx = document.getElementById('total3-graph').getContext('2d');
+var total3_graph = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 88],
+            backgroundColor: [
+                '#442237',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                '#FE0000',
+                'rgba(54, 162, 235, 1)',
+
+            ],
+            borderWidth: 1,
+            maintainAspectRatio: false,
+        }]
+    },
+
+});
