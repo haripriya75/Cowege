cbd269e60d190567e8caf5f5d206a72467133d5c
haripriya75 <haripriyaradhika28@gmail.com>
2021-09-05 00:18:31 +0530

    kaam kro

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ base_url = 'https://covege.herokuapp.com';
 // base_url = 'http://localhost';
 
 
-function loginUser(){
+function loginUser() {
     var data = JSON.stringify({
         "email": document.getElementById("loginEMAIL").value,
         "password": document.getElementById("loginPASSWORD").value,
@@ -11,23 +11,33 @@ function loginUser(){
     var config = {
         method: 'post',
         url: base_url + '/api/auth/authenticate',
-        headers: { 
+        headers: {
             'Content-Type': 'application/json'
         },
-        data : data
+        data: data
     };
 
     axios(config)
-        .then(function (response) {
-        console.log(JSON.stringify(response.data));
-    })
-        .catch(function (error) {
-        console.log(error);
-    });
+        .then(function(response) {
+            // var checkbox = document.getElementById('switch');
+            // console.log(checkbox)
+            // checkbox.addEventListener('change', function() {
+            //     if (this.checked) {
+            //         window.location.replace('/d-dashboard.html')
+            //     } else {
+            //         window.location.replace('/l-dashboard.html')
+
+            //     }
+            // })
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 
 }
 
-function registerUser(){
+function registerUser() {
     var data = JSON.stringify({
         "name": document.getElementById("signupNAME").value,
         "email": document.getElementById("signupEMAIL").value,
@@ -37,21 +47,24 @@ function registerUser(){
     var config = {
         method: 'post',
         url: base_url + '/api/auth/register',
-        headers: { 
+        headers: {
             'Content-Type': 'application/json'
         },
-        data : data
+        data: data
     };
 
     axios(config)
-    .then(function (response) {
-    console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-    console.log(error);
-    });
+        .then(function(response) {
+            console.log(JSON.stringify(response.data));
+            window.location.replace('/form.html')
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+
 }
 
-function googleLogin(){
+function googleLogin() {
     location.replace("https://covege.herokuapp.com/api/auth/google-login")
 }
